25eda0b09d9be85e31e2289c5aa6fe069a672d7d
Edmilson <scovinowork@gmail.com>
2021-08-10 23:01:29 -0300

    variaveis

---
+++ pythonTutorialW3/variaveis/variaveis.py
@@ -0,0 +1,36 @@
+#O Python, nao necessita  de declarar uma variavel, no momento que atribui o valor na variavel, ele ja reconhece.
+
+x = 5
+y = "Nao precisa declarar"
+
+print(x)
+print(y)
+
+#Mas se caso eu precisar saber o tipo da variavel, basta apenas colocar 'type()'
+
+x = 120
+y = "The Python"
+z = 2.0
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Strings podemos declarar com aspas simples e aspas duplas
+
+x = "Vanessa"
+print(x)
+u = 'Brenda'
+print(u)
+
+#Nomes de variaveis, tem diferenca sendo minuscula e maiuscula , entao preste atenca na hora declarar
+
+a = "minuscula"
+A = 'MAIUSCULA'
+
+print(a)
+print(A)
+
+print(type(a))
+print(type(A))
