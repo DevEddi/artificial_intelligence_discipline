9970fbe3eaaffa90667d37080422c6abdc94c99b
Edmilson <scovinowork@gmail.com>
2021-08-10 23:12:04 -0300

    variaveis multiplas

---
+++ pythonTutorialW3/variaveis/variaveisMultiplas.py
@@ -0,0 +1,33 @@
+#Podemos declarar variaveis em uma mesma linha
+
+x, y, z = "Orange", "Apple", "Pineapple"
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Podemos declarar um valor para varias variaveis
+
+x = y = z = "Estou aqui, ali e aqui"
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Descompacte uma lista
+
+fruits = ["apple", "banana", "cherry"]
+
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
