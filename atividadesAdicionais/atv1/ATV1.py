a142b671906fbd40b11fb184f17860ca87b6a726
Edmilson <scovinowork@gmail.com>
2021-08-15 14:34:54 -0300

    Atv

---
+++ atividadesAdicionais/atv1/ATV1.py
@@ -0,0 +1,10 @@
+print("Soma de de valores: ")
+print("Digite o primeiro valor: ")
+num1 = int(input())
+
+print("Digite o segundo valor: ")
+num2 = int(input())
+
+resultado = num1 + num2
+
+print('A soma dos valores é ', resultado)
