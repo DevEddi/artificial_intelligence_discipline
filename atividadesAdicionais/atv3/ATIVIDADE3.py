99f54869d6177d25b0df82247d7961ee739106bc
Edmilson <scovinowork@gmail.com>
2021-08-21 10:30:45 -0300

    atv3

---
+++ atividadesAdicionais/atv3/ATIVIDADE3.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que:
+◦
+Solicite ao usuário o número de avaliações de um
+aluno;
+◦
+Solicite ao usuário o número de alunos;
+◦
+Para cada aluno, solicite o nome e as notas
+necessárias. Armazene num dicionário em que a
+chave é o nome e os valores são as notas;
+◦
+Após a leitura de todas as notas, imprima o nome
+do aluno e sua média final;
+◦
+Calcule a média da turma (baseado na média final)
+"""
